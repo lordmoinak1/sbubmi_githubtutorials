306a07641fbcbf21ccae9cc5aef8d113b65fe495
Moinak Bhattacharya <53391762+lordmoinak1@users.noreply.github.com>
2022-11-21 09:11:51 -0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from datasets import *
+from models import *
+
+
+def train(epochs):
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+  
+  for epoch in range(epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+           
+if __name__ == "__main__":
+  trainloader, testloader = cifar10_dataset()
+  net = ViT(
+      image_size = 256,
+      patch_size = 32,
+      num_classes = 1000,
+      dim = 1024,
+      depth = 6,
+      heads = 16,
+      mlp_dim = 2048,
+      dropout = 0.1,
+      emb_dropout = 0.1
+  )
+            
+ 
+
+  
